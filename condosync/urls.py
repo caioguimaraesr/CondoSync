be5095679c63e37f0ba276c9acdc798b7c4a38cf
caioguimaraesr <caioguimaraesrocha70@gmail.com>
2025-03-30 00:23:01 -0300

    adicionando um app_name para o app condosync

--- condosync/urls.py
+++ condosync/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'condosync'
+
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
 ]
