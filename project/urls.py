1bd1b739d17e7934a90011c20d3989669429f1d7
caioguimaraesr <caioguimaraesrocha70@gmail.com>
2025-03-30 00:29:15 -0300

    colocando o include para o novo app userauth na urls do projeto

--- project/urls.py
+++ project/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('condosync.urls'))
+    path('', include('userauth.urls')),
+    path('condosync/', include('condosync.urls')),
 ]
