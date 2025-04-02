426965681315e880646cd34d152df33d63d6bb26
caioguimaraesr <caioguimaraesrocha70@gmail.com>
2025-04-02 20:37:00 -0300

    cadastando os models apartamento e boleto no admin

--- condosync/admin.py
+++ condosync/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Boleto, Apartamento
 # Register your models here.
+
+@admin.register(Apartamento)
+class ApartamentoAdmin(admin.ModelAdmin):
+    ...
+
+@admin.register(Boleto)
+class BoletoAdmin(admin.ModelAdmin):
+    ...
