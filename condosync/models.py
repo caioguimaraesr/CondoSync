e2ddc8efacdf6a96e9a05b4dc70210308e3d2672
caioguimaraesr <caioguimaraesrocha70@gmail.com>
2025-04-02 20:38:14 -0300

    Criando os models para a aba de boletos

--- condosync/models.py
+++ condosync/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Apartamento(models.Model):
+    numero = models.CharField(max_length=10, unique=True)
+    morador = models.OneToOneField(User, on_delete=models.CASCADE, related_name="apartamento")
+
+    def __str__(self):
+        return f"APTO{self.numero}: {self.morador.username}"
+
+class Boleto(models.Model):
+    apartamentos = models.ForeignKey(Apartamento, on_delete=models.CASCADE)
+    mes_referencia = models.CharField(max_length=26)
+    arquivo = models.FileField(upload_to='documents/boletos/')
+
+    def __str__(self):
+        return f"{self.mes_referencia}: APTO{self.apartamentos.numero}"
