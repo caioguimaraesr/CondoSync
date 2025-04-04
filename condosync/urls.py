dc79ac9e52011c3ec42ce8e78f7fbd70f9a44896
caioguimaraesr <caioguimaraesrocha70@gmail.com>
2025-04-04 14:48:57 -0300

    config do back para a funcionalidade Avisos

--- condosync/urls.py
+++ condosync/urls.py
@@ -5,5 +5,13 @@ app_name = 'condosync'
 
 urlpatterns = [
     path('home/', views.home, name='home'),
-    path('boletos/', views.boletos, name='boletos')
+    
+    #Boletos
+    path('boletos/', views.boletos, name='boletos'),
+
+    ## Avisos
+    path('avisos/', views.avisos, name='avisos'),
+    path('avisos/create', views.create_avisos, name='create_avisos'),
+    path('avisos/edit/<int:id>', views.edit_avisos, name='edit_avisos'),
+    path('avisos/delete/<int:id>', views.delete_avisos, name='delete_avisos'),
 ]
--- condosync/views.py
+++ condosync/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required
-from .models import Boleto, Apartamento
+from django.contrib.auth.decorators import login_required, user_passes_test
+from .models import Boleto, Apartamento, Aviso
 from django.contrib import messages
+from django.utils import timezone
 # Create your views here.
+def is_admin(user):
+    if not user.is_superuser:
+        return False 
+    return True
 
 @login_required(login_url='userauth:login_register')
 def home(request):
     return render(request, 'condosync/pages/home.html')
 
+########################### Boletos ###################################
 @login_required(login_url='userauth:login_register')
 def boletos(request):
     apartamento = get_object_or_404(Apartamento, morador=request.user)
@@ -39,3 +45,49 @@ def boletos(request):
         'boletos': boletos,
         'apartamentos': apartamentos 
     })
+
+############################### Avisos ##############################################
+@login_required(login_url='userauth:login_register')
+def avisos(request):
+    avisos = Aviso.objects.order_by('-data_postagem')
+    return render(request, "condosync/pages/avisos/avisos.html", context={
+        'avisos':avisos
+    })
+
+@login_required(login_url='userauth:login_register')
+@user_passes_test(is_admin, login_url='condosync:avisos')
+def create_avisos(request):
+    if request.method == 'POST':
+        titulo = request.POST.get('titulo')
+        conteudo = request.POST.get('conteudo')
+        if titulo and conteudo:
+            Aviso.objects.create(titulo=titulo, conteudo=conteudo, data_postagem=timezone.now())
+            return redirect('condosync:avisos')
+    return render(request, 'condosync/pages/avisos/create_avisos.html', context={
+        'acao': 'Adicionar'
+        })
+
+@login_required(login_url='userauth:login_register')
+@user_passes_test(is_admin, login_url='condosync:avisos')
+def edit_avisos(request, id):
+    aviso = get_object_or_404(Aviso, id=id)
+    if request.method == 'POST':
+        aviso.titulo = request.POST.get('titulo')
+        aviso.conteudo = request.POST.get('conteudo')
+        aviso.save()
+        return redirect('condosync:avisos')
+    return render(request, 'condosync/pages/avisos/edit_avisos.html', context={
+        'aviso': aviso, 'acao': 'Editar'
+        })
+
+@login_required(login_url='userauth:login_register')
+@user_passes_test(is_admin, login_url='condosync:avisos')
+def delete_avisos(request, id):
+    aviso = get_object_or_404(Aviso, id=id)
+    if request.method == 'POST':
+        aviso.delete()
+        messages.success(request, 'Aviso excluído com sucesso!!')
+        return redirect('condosync:avisos')
+    return render(request, 'condosync/pages/avisos/delete_avisos.html', context={
+        'aviso': aviso
+        })
