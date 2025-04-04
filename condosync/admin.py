d0df134ef4e2131c43bad59c3d4b2426bd07e6b3
caioguimaraesr <caioguimaraesrocha70@gmail.com>
2025-04-04 14:45:36 -0300

    Adicionando o model no /admin

--- condosync/admin.py
+++ condosync/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Boleto, Apartamento
+from .models import Boleto, Apartamento, Aviso
 # Register your models here.
 
 @admin.register(Apartamento)
@@ -8,4 +8,8 @@ class ApartamentoAdmin(admin.ModelAdmin):
 
 @admin.register(Boleto)
 class BoletoAdmin(admin.ModelAdmin):
+    ...
+
+@admin.register(Aviso)
+class AvisoAdmin(admin.ModelAdmin):
     ...
