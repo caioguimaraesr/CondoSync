f2881059422e90bb73d94e6d0b4f5b65d24343bb
caioguimaraesr <caioguimaraesrocha70@gmail.com>
2025-04-03 22:14:21 -0300

    Alterando o model de apartamento

--- condosync/models.py
+++ condosync/models.py
@@ -4,10 +4,12 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Apartamento(models.Model):
     numero = models.CharField(max_length=10, unique=True)
-    morador = models.OneToOneField(User, on_delete=models.CASCADE, related_name="apartamento")
+    morador = models.OneToOneField(User, on_delete=models.CASCADE, related_name="apartamento",null=True, blank=True)
 
     def __str__(self):
-        return f"APTO{self.numero}: {self.morador.username}"
+        if self.morador:
+            return f"APTO{self.numero}: {self.morador.username}"
+        return f"APTO{self.numero}"
 
 class Boleto(models.Model):
     apartamentos = models.ForeignKey(Apartamento, on_delete=models.CASCADE)
