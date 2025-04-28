5c0272cefe1a6bc4d4c3706c4e527898b9889756
rafaferraz05 <rafaelferraz.silva05@gmail.com>
2025-04-28 11:49:17 -0300

    adicionando os caminhos para as novas funcionalidades

--- condosync/urls.py
+++ condosync/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'condosync'
 
 urlpatterns = [
+    ## Home page
     path('home/', views.home, name='home'),
     
     #Boletos
@@ -21,11 +22,18 @@ urlpatterns = [
     path('encomendas/editar/<int:id>/', views.edit_encomendas, name='edit_encomendas'),
     path('encomendas/delete/<int:id>/', views.delete_encomendas, name='delete_encomendas'),
 
-    ## Carros
+    ## Veículos
     path('veiculos/', views.meus_veiculos, name="meus_veiculos"),
     path('veiculos/adicionar_veiculos/', views.adicionar_veiculos, name="adicionar_veiculos"),
     path('veiculos/gerenciar_veiculos/', views.gerenciar_veiculos, name="gerenciar_veiculos"),
     path('veiculos/editar_veiculos/<int:id>/', views.editar_veiculos, name="editar_veiculos"),
     path('veiculos/deletar_veiculos/<int:id>/', views.deletar_veiculos, name="deletar_veiculos"),
-    path('veiculos/veiculos/admin', views.veiculos_admin, name="veiculos_admin"),
+    path('veiculos/admin/', views.veiculos_admin, name="veiculos_admin"),
+
+    ## Ocorrências
+    path('ocorrencias/', views.ocorrencias, name='ocorrencias'),
+    path('ocorrencias/create/', views.create_ocorrencias, name='create_ocorrencia'),
+    path('ocorrencias/edit/<int:id>/', views.edit_ocorrencias, name='edit_ocorrencias'),
+    path('ocorrencias/status/<int:id>/', views.status_ocorrencias, name='status_ocorrencias'),
+    path('ocorrencias/delete/<int:id>/', views.delete_ocorrencias, name='delete_ocorrencias'),
 ]
